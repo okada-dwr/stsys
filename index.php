107fd871ef14e5db7c9a2800c3e82f6a1c2906d3
okada-dwr <94820972+okada-dwr@users.noreply.github.com>
2021-12-17 18:04:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $year = date("Y");
 
 $Holidays_array = getHolidays($year);
 
+$prev_show = 1; //前月の表示・非表示判断フラグ
+$today_show = date('Ym');; //前月の表示・非表示判断に使う
 //前月・次月リンクが選択された場合は、GETパラメーターから年月を取得
 if (isset($_GET['ym'])) {
     $ym = $_GET['ym'];
@@ -30,11 +32,29 @@ $today = date('Y-m-j');
 //カレンダーのタイトルを作成　例）2020年10月
 $html_title = date('Y年n月', $timestamp); //date(表示する内容,基準)
 
-//前月・次月の年月を取得
-//strtotime(,基準)
-$prev = date('Y-m', strtotime('-1 month', $timestamp));
-$next = date('Y-m', strtotime('+1 month', $timestamp));
 
+//strtotime(,基準)
+$prev = date('Y-m', strtotime('-1 month', $timestamp)); //前月の年月を取得
+$next = date('Y-m', strtotime('+1 month', $timestamp)); //来月の年月を取得
+
+//前月・次月リンクが選択された場合は、GETパラメーターから年月を取得
+if (isset($_GET['ym'])) {
+    $ym = $_GET['ym'];
+    $ym_show = $ym;
+    $ym_show = str_replace('-', '', $ym_show);
+
+    //echo $today_show, '<', $ym_show;
+
+    if ($today_show < $ym_show) { //ユーザー選択日付が今日の日付より大きい場合、前月遷移を表示する。
+        $prev_show = '<h3 style="font-size: 1.0rem;"><a href="?ym=' . $prev . '">&lt;&lt;前月 </a>' . $html_title . '<a href="?ym=' . $next . '"> 来月&gt;&gt;</a></h3>';
+    } else {   //ユーザー選択日付が今日の日付と同じ場合、前月遷移を表示しない。
+        $prev_show =  '<h3 style="font-size: 1.0rem;">' . $html_title . '<a href="?ym=' . $next . '"> 来月&gt;&gt;</a></h3>';
+    }
+} else { //ユーザー選択日付が今日の日付と同じ場合、前月遷移を表示しない。
+    //今月の年月を表示
+    $ym = date('Y-m');
+    $prev_show = '<h3 style="font-size: 1.0rem;">' . $html_title . '<a href="?ym=' . $next . '"> 来月&gt;&gt;</a></h3>';
+}
 
 //該当月の日数を取得
 $day_count = date('t', $timestamp);
@@ -48,6 +68,7 @@ $week = '';
 $week_none = '';
 $youbi_kana = '';
 $count = 0;
+$ketugou = [];
 //第１週目：空のセルを追加
 //str_repeat(文字列, 反復回数)
 /*$week .= str_repeat('<td></td>', $youbi);*/
@@ -103,8 +124,78 @@ for ($day = 1; $day <= $day_count; $day++, $youbi++) {
 
     /*$reservation = reservation(date("Y-m-d", strtotime($date)), $reservation_array);*/
 
+    $month_db = 1; //月
+    $start_day = [2, 8, 13, 30];
+    $end_day = [2, 10, 14, 2];
+    $keisan = 0;
 
+    $key_count = 0;
+    $count = 0;
+    $higawari = 0;
 
+    for ($i = 0; $i < count($start_day); $i++) { //何日結合するか調べる（8日～10日の場合２つ）
+        //echo $count . '=' . $end_day[$i] . '-' . $start_day[$i] . 'finish';
+        $count = $end_day[$i] - $start_day[$i];
+        if ($count < 0) {
+            switch ($month_db) {
+
+                case 1 or 3 or 5 or 7 or 8 or 10 or 12:
+                    $higawari = 31 - $start_day[$i];
+                    if ($higawari === 0) {
+                        for ($z = 0; $z < $end_day[$i]; $z++) {
+                            $coun1 += 1;
+                            $ketugou[$key_count] = array($start_day[$i] => 1 + $coun1 - 1); //結合される部分作成(8日なら9日と10日が結合される
+                            $key_count += 1;
+                        }
+                    } else {
+                        for ($z = 0; $z < $higawari; $z++) {
+                            $coun1 += 1;
+                            $ketugou[$key_count] = array($start_day[$i] => $start_day[$i] + $coun1); //結合される部分作成(8日なら9日と10日が結合される
+                            $key_count += 1;
+                        }
+                        for ($z = 0; $z < $end_day[$i]; $z++) {
+
+                            $ketugou[$key_count] = array($start_day[$i] => 1 + $coun1 - 1); //結合される部分作成(8日なら9日と10日が結合される
+                            $key_count += 1;
+                            $coun1 += 1;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    break;
+                case 4 or 6 or 9 or 11:
+                    break;
+                default:
+            }
+        } else {
+            for ($z = 0; $z < $count; $z++) {
+                $coun1 += 1;
+                //echo $start_day[$z] . '+' . $coun1 . '=';
+
+                //予約スタート日が1桁か2桁か調べる。
+                //後のforeach等の処理で$ketugouを使う際、[.2桁]を切り取り消すのでその都合。
+                // if (strlen($start_day[$i]) === 1) {
+                //     $group_day = "0";
+                // } else {
+                //      $group_day = "";
+                //  }
+
+                //｛0:8=>9.08,0:8=>10.08｝後ろの.08は8日から10日までのグループであることを示す。
+                $ketugou[$key_count] = array($start_day[$i] => $start_day[$i] + $coun1); //結合される部分作成(8日なら9日と10日が結合される
+                //$ketugou[$key_count] = array($start_day[$i] => $start_day[$i] + $coun1 . $group_day . $start_day[$i]); //結合される部分作成(8日なら9日と10日が結合される
+                //$ketugou[$key_count] = array($start_day[$i] => $start_day[$i] + $coun1); //結合される部分作成(8日なら9日と10日が結合される
+                $key_count += 1;
+            }
+        }
+
+        $z = 0;
+        $coun1 = 0;
+    }
+
+    $skip_check = 0;
+    $colspan_count = 0;
+    $finish_key_count = 0;
 
     if ($today == $date) {
         //もしその日が今日なら
@@ -119,12 +210,84 @@ for ($day = 1; $day <= $day_count; $day++, $youbi++) {
         /*} elseif (reservation(date("Y-m-d", strtotime($date)), $reservation_array)) {
         $week .= '<td>' . $day . $reservation;*/
     } else {
+        $skip_check = 0;
         //上２つ以外なら
-        $week .= '<td>' . $day;
-        $week_none .= '<td>';
+        //foreach ($ketugou as $ketugou1) { //$dayが結合される日付か確認する(0:飛ばさない 1:飛ばす)
+        foreach ($ketugou as $key => $ketugou1) { //予約日のキーを探して結合数を数える
+            foreach ($ketugou1 as $key => $ketugou3) { //予約日のキーを探して結合数を数える
+                if ($day === $ketugou3) {
+                    $skip_check = 1;
+                    break;
+                }
+            }
+            //$str = $ketugou1; //文字列
+            //$cut = 2; //カットしたい文字数
+            //$ketugou1 = substr($str, 0, strlen($str) - $cut); // 808の場合右から08を切り取り8にする
+        }
+
+        //$i = [1 => "a", 1 => "b", 2 => "c"];
+
+        if ($skip_check === 0) { //結合される日付は飛ばす(0:飛ばさない 1:飛ばす)
+            $yoyakubi_ari = 0;
+            //この段階では予約のない日か予約日の先頭日付かどちらかになる
+            foreach ($start_day as $start_day) {
+                $key_name = 0;
+                if ($yoyakubi_ari === 0) {
+                    if ($day === $start_day) { //予約日かそうでないか判断する
+                        $yoyakubi_ari = 1;
+                        $key_name = $start_day;
+                        foreach ($ketugou as $key => $ketugou2) { //予約日のキーを探して結合数を数える
+                            foreach ($ketugou2 as $key => $ketugou2) { //予約日のキーを探して結合数を数える
+                                //$key_count = array_keys($ketugou2);
+                                if ($day === $key) {
+                                    $finish_key_count += 1;
+                                }
+                            }
+                            //$key_count = array_keys($ketugou2);
+                            //if ($day === $key) {
+                            //    $finish_key_count += 1;
+                            //}
+                        }
+                        if ($colspan_count <> 0) { //結合する数が０ではなかったら
+                            $week .= '<td>' . $day;
+                            $week_none .= '<td class="course_type" colspan="' . $finish_key_count + 1 . '">';
+                        } else { //結合する数が０なら普通に表示する
+                            $week .= '<td>' . $day;
+                            $week_none .= '<td class="course_type" colspan="' . $finish_key_count + 1 . '">';
+                        }
+                        //} else { //予約日でないから普通に表示する
+                        //$week .= '<td>' . $day;
+                        //$week_none .= '<td>';
+                    }
+                }
+            }
+            if ($yoyakubi_ari === 0) {
+                $week .= '<td>' . $day;
+                $week_none .= '<td>';
+            }
+            //  if ($day === $start_day) { //予約日の始まりの日なら
+            // foreach ($ketugou as $ketugou) { //予約日のキーを探して結合数を数える
+            // $key_count = array_keys($ketugou);
+            //}
+            // if ($colspan_count <> 0) { //結合する数が０ではなかったら
+            //$week .= '<td>' . $day;
+            //  $week_none .= '<td class="course_type" colspan="' . $ketugou + 1 . '">';
+            //  } else { //結合する数が０なら普通に表示する
+            //    $week .= '<td>' . $day;
+            //    $week_none .= '<td class="course_type" colspan="' . $ketugou + 1 . '">';
+            //  }
+            //  } else {
+            //     $week .= '<td>' . $day;
+            //     $week_none .= '<td>';
+            // }
+            $week .= '</td>';
+            $week_none .= '</td>';
+        } else {
+            $week .= '<td>' . $day;
+            $week .= '</td>';
+        }
     }
-    $week .= '</td>';
-    $week_none .= '</td>';
+
 
 
     /*if ($youbi % 7 == 6 || $day == $day_count) { //週終わり、月終わりの場合
@@ -345,6 +508,7 @@ function reservation($date, $reservation_array)
 </head>
 
 <body id=""> 　　　　
+    <a href="stsys02.php">a</a>
     　　　　<div class="title1">
         <h1 style="text-align: center;text-decoration:underline;">予約表示画面</h1>
         <div class="explanation" style="margin-left: 130px;margin-right: 130px;border:1px solid;padding-left:5px;">
@@ -362,11 +526,24 @@ function reservation($date, $reservation_array)
         <table class="table table-bordered">
             <tr>
                 <td>受講人数（※10名まで）</td>
-                <td>▼</td>
+                <td><select name="example">
+                        <option value="1">1人</option>
+                        <option value="2">2人</option>
+                        <option value="3">3人</option>
+                        <option value="4">4人</option>
+                        <option value="5">5人</option>
+                        <option value="6">6人</option>
+                        <option value="7">7人</option>
+                        <option value="8">8人</option>
+                        <option value="9">9人</option>
+                        <option value="10">10人</option>
+                    </select></td>
             </tr>
             <tr>
                 <td rowspan="2">
-                    <h3 style="font-size: 1.0rem;"><a href="?ym=<?php echo $prev; ?>">&lt;&lt;前月 </a><?php echo $html_title; ?><a href="?ym=<?php echo $next; ?>"> 来月&gt;&gt;</a></h3>
+                    <?php
+                    echo $prev_show;
+                    ?>
                 </td>
                 <?php
                 echo $week;
@@ -597,7 +774,7 @@ function reservation($date, $reservation_array)
             </div>
         </div>
     </div>
-    <div class="pay_explanation">
+    <div id="pay_explanation">
         <div class="pay_explanation_left" style="font-size: 2rem;">
             <b>受講料の支払について</b>
             <p>●受講料は受講日までに、こちらから送付する「運転技能講習のご案内」に記載されている、<br>口座へ振込みをお願いいたします。なお、手数料はお客様負担になります。</p>
