27e7cfb33662be71c9167a39b07f7bd937213d38
okada-dwr <94820972+okada-dwr@users.noreply.github.com>
2022-01-21 16:28:40 +0900

    Create stsys03.php

---
+++ stsys03.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset=" UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+</head>
+
+<body>
+    <a href="index.php">stsys01</a>
+    <a href="stsys02.php">stsys02</a>
+    <a href="stsys03.php">stsys03</a>
+    <a href="stsys04.php">stsys04</a>
+
+    <table border="1" style="border-collapse:collapse" ;>
+        <tr>
+            <td>講習名</td>
+            <td><select name="" id="">
+                    <option value=""></option>
+                    <option value="1">1</option>
+                </select></td>
+        </tr>
+    </table>
+    <table border="1" style="border-collapse:collapse" ;>
+        <tr>
+            <td>講習期間</td>
+            <td><input type="text"></td>
+            <p>~</p>
+            <input type="text">
+        </tr>
+        <button>検索</button>
+        <button>キャンセル </button>
+        <button>Excel出力</button>
+    </table>
+    <table border="1" style="border-collapse:collapse" ;>
+        <tr style="background-color: lightskyblue;">
+            <th rowspan="2"><input type="checkbox"></th>
+            <th rowspan="2">助成金</th>
+            <th rowspan="2">コース</th>
+            <th rowspan="2">郵送希望</th>
+            <th rowspan="2">受講者指名</th>
+            <th rowspan="2">個人電話番号</th>
+            <th rowspan="2">住所</th>
+            <th rowspan="2">講習開始日</th>
+            <th rowspan="2">講習終了日</th>
+            <th colspan="6">会社情報</th>
+        </tr>
+        <tr>
+            <th>会社名</th>
+            <th>申請者氏名</th>
+            <th>住所</th>
+            <th>電話番号</th>
+            <th>FAX番号</th>
+        </tr>
+        <tr>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+            <td style="height: 50px;"></td>
+        </tr>
+    </table>
+    <p>※「申込書有無」「入金日」「金額」のチェック後の登録は、「当日受付チェックツール.xlsm」を使用し登録を行ってください。
+    </p>
+</body>
+</body>
+
+</html>
