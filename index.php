815a4d8ed1f0415dab51772234a5f17efb458763
okada-dwr <94820972+okada-dwr@users.noreply.github.com>
2022-01-21 18:03:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php 
 echo 'heoo';
+echo  '<a href="index.php">stsys01</a>';
+echo  '<a href="stsys02.php">stsys02</a>';
+echo  '<a href="stsys03.php">stsys03</a>';
+echo  '<a href="stsys04.php">stsys04</a>';
 ?>
