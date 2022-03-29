12330cfff9a4b8383bbb99690c659c8ba4b8ede7
okada-dwr <94820972+okada-dwr@users.noreply.github.com>
2022-03-29 10:35:21 +0900

    Update index.php

--- index.php
+++ index.php
@@ -659,6 +659,7 @@ function display_to_Holidays($date, $Holidays_array)
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHPカレンダー</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
