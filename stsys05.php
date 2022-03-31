ac85bd02372496abf26c4a40e0d3e41e441b5f43
okada-dwr <94820972+okada-dwr@users.noreply.github.com>
2022-03-31 13:17:57 +0900

    Create stsys05.php

---
+++ stsys05.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.2.0/js/tableexport.min.js" integrity="sha512-XmZS54be9JGMZjf+zk61JZaLZyjTRgs41JLSmx5QlIP5F+sSGIyzD2eJyxD4K6kGGr7AsVhaitzZ2WTfzpsQzg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="/style5.css">
+</head>
+
+<body>
+    <a href="index.php">stsys01</a>
+    <a href="stsys02.php">stsys02</a>
+    <a href="stsys03.php">stsys03</a>
+    <a href="stsys04.php">stsys04</a>
+    <a href="stsys05.php">stsys05</a>
+</body>
+<div class="container">
+    <div class="main">
+        <section id="complete">
+
+            <div class="pagetitle">
+                <div class="sideline">
+                    <!-- <h2>フォーム送信完了</h2> -->
+                    <!-- <p class="subtitle fon">SEND COMPLETELY</p> -->
+                </div>
+                <div class="pagetitle_line"></div>
+            </div>
+            <!--pagetitle-->
+
+            <p class="title">送信が完了しました</p>
+            <p class="copy">フォーム内容をご送信頂きまして誠にありがとうございます。</p>
+            <!-- <p class="copy">フォーム内容をご送信頂きまして誠にありがとうございます。<br>担当者よりご案内させていただきます。</p> -->
+            <div class="btns"><a href="./index.php">
+                    <div class="btn"><span>ホームへ戻る</span></div>
+                </a></div>
+            <!--btns-->
+
+
+        </section>
+        <!--complete-->
+    </div>
+</div>
+
+</html>
