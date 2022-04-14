e27c77f03fe99f39e194594343e8288dfdde3589
okada-dwr <94820972+okada-dwr@users.noreply.github.com>
2022-04-14 11:13:15 +0900

    Add files via upload

---
+++ stsys07.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.2.0/js/tableexport.min.js" integrity="sha512-XmZS54be9JGMZjf+zk61JZaLZyjTRgs41JLSmx5QlIP5F+sSGIyzD2eJyxD4K6kGGr7AsVhaitzZ2WTfzpsQzg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="/style5.css">
+</head>
+
+<body>
+    <a href="index.php">stsys01</a>
+    <a href="stsys02.php">stsys02</a>
+    <a href="stsys03.php">stsys03</a>
+    <a href="stsys04.php">stsys04</a>
+    <a href="stsys05.php">stsys05</a>
+</body>
+<div class="container">
+    <div class="main">
+        <section id="complete">
+            <p class="title">エラーが発生しました。</p>
+            <p class="call">お手数をおかけしますが、「0773-76-0652」高岸までご連絡ください。</p>
+        </section>
+    </div>
+</div>
+
+</html>
